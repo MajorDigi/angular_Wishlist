16c9d83ec28eec854640cca23276a376bfc0cfc5
Major McNeil <MMcNeil@psdata.cc>
2024-11-13 13:31:42 -0500

    Modified angular.json and app.component.ts; added new file wishItem.ts in src/shared/modules

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { WishItem } from '../shared/modules/wishItem';
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -9,5 +9,10 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  items = [
+    new WishItem('Learn Angular'),
+    new WishItem('Get Coffee', true),
+    new WishItem('Find grass that cuts itself')
+  ]
   title = 'angular_Wishlist';
 }
