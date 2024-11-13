969f51f581ea47357395072927b9b6dba32b8ae8
Major McNeil <MMcNeil@psdata.cc>
2024-11-13 16:51:26 -0500

    Added checkbox functionality to wish items, resolved ngFor directive error by importing CommonModule, updated app.component.html to include checkboxes, app.component.ts to handle checkbox state changes with a new toggleChecked method, modified WishItem model to include isChecked property, styled components in app.component.css, and made updates to package.json, package-lock.json, and src/index.html for proper integration.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { WishItem } from '../shared/modules/wishItem';
+import { CommonModule } from '@angular/common'; // Import CommonModule
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule], //Add CommonModule
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
