4be5ad0f8b28d75bf9215c865b28ed13e46d5d05
Major McNeil <MMcNeil@psdata.cc>
2024-11-14 16:32:23 -0500

    Adjusted app.component.ts 'standalone: true, // This makes AppComponent standalone'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,22 @@ import { RouterOutlet } from '@angular/router';
 import { WishItem } from '../shared/modules/wishItem';
 import { CommonModule } from '@angular/common'; // Import CommonModule
 
+// @Component({
+//   selector: 'app-root',
+//   standalone: true,
+//   imports: [RouterOutlet, CommonModule], //Add CommonModule
+//   templateUrl: './app.component.html',
+//   standalone: true, // This makes AppComponent standalone
+//   styleUrl: './app.component.css'
+// })
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet, CommonModule], //Add CommonModule
+  standalone: true, // This makes AppComponent standalone
+  imports: [RouterOutlet, CommonModule], // Add CommonModule and RouterOutlet
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'] // Corrected to styleUrls
 })
+
 export class AppComponent {
   // items = [
   //   new WishItem('Learn Angular'),
